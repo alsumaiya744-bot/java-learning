f6d99584aeb8c7f4fc1bcb70ff5c5c344f7e242a
Sumaiya <alsumaiya744@gmail.com>
2025-12-23 18:37:49 +0400

    Updated Unary operator

--- operator HW5/UnaryOperator.java
+++ operator HW5/UnaryOperator.java
@@ -3,10 +3,14 @@ class UnaryOperator
 	public static void main(String[] args)
 	{
 		int a=5;
-		System.out.println("After a++ ="+(a++));
-		System.out.println("After ++a ="+(++a));
-		System.out.println("After a-- ="+(a--));
-		System.out.println("After --a ="+(--a));
+		a++;
+		System.out.println("After a++ ="+a);
+		++a;
+		System.out.println("After ++a ="+a);
+		a--;
+		System.out.println("After a-- ="+a);
+		--a;
+		System.out.println("After --a ="+a);
 	}
 	
 }
